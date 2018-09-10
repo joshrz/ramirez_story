2d2c00137d2a4e133163d3fecd89e1cc5a2f7308
Josh Ramirez <joshuaram.21950@redlandsschools.net>
2018-09-10 15:25:51 +0000

    testing

---
+++ second.py
@@ -0,0 +1,9 @@
+"""
+ triple QUOTES will allow me to comment out multiple lines
+ 
+write a program that will ask the user what there age is and then 
+ determine if they are old enough to vote or not respond properly
+
+"""
+num = int(input("how old are you?"))
+age 
